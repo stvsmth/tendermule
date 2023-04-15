c5657e4488ec842c0f8d7e81955c55ee14880fa7
stvsmth <stvsmth@gmail.com>
2023-04-14 21:36:29 -0400

    Move words into binary, use static text.
    Yay compilers.
    
    Passes the most basic manual testing, time to get medieval on this thing
    and write some real tests, maybe even investigate fuzzing.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use rand::Rng;
-use std::fs;
+
+mod words;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -21,11 +22,8 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let adjectives_file = "words-adj.txt";
-    let adjs = read_file(adjectives_file);
-
-    let nouns_file = "words-nouns.txt";
-    let nouns = read_file(nouns_file);
+    let adjs = words::adjs::ADJS;
+    let nouns = words::nouns::NOUNS;
 
     let mut max_length = args.max_length - args.prefix.len() - args.suffix.len();
     let random_adj = choose_word(adjs, max_length);
@@ -48,8 +46,8 @@ fn main() {
 
 /// Given a vector of words, choose a random word that is less than or equal to
 /// the given max length.
-fn choose_word(words: Vec<String>, max_length: usize) -> String {
-    let filtered_words: Vec<String> = words
+fn choose_word(words: &[&str], max_length: usize) -> String {
+    let filtered_words: Vec<&str> = words
         .iter()
         .filter(|word| word.len() <= max_length)
         .cloned()
@@ -58,14 +56,7 @@ fn choose_word(words: Vec<String>, max_length: usize) -> String {
     let mut rng = rand::thread_rng();
     let random_index = rng.gen_range(0..=filtered_words.len() - 1);
 
-    filtered_words[random_index].clone()
-}
-
-fn read_file(filename: &str) -> Vec<String> {
-    let contents =
-        fs::read_to_string(filename).unwrap_or_else(|_| panic!("Failed to read file {}", filename));
-
-    contents.split_whitespace().map(|s| s.to_string()).collect()
+    filtered_words[random_index].to_string()
 }
 
 fn capitalize_first_char(s: &str) -> String {
---
+++ src/words/mod.rs
@@ -0,0 +1,2 @@
+pub mod adjs;
+pub mod nouns;
