a678d9d8563d2b89ddb7d1e05c181e324d43acc2
stvsmth <stvsmth@gmail.com>
2023-04-29 11:20:17 -0600

    Add test to ensure we're using input words.
    Mostly a test to ensure we don't end up with empty strings or other kind
    of odd regression bugs.

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -15,8 +15,17 @@ fn test_count_generates_unique_values() {
         max_length,
     );
     let results = ids.into_iter().collect::<Vec<String>>();
+    // Ensure we get exactly 2 words
     assert_eq!(results.len(), 2);
     let id_1 = results[0].clone();
     let id_2 = results[1].clone();
+
+    // Ensure the words are not the same
     assert!(id_1 != id_2);
+
+    // Ensure the generated ids contain an adj and noun
+    assert!(adjs.iter().any(|&adj| id_1.to_lowercase().contains(adj)));
+    assert!(nouns.iter().any(|&noun| id_1.to_lowercase().contains(noun)));
+    assert!(adjs.iter().any(|&adj| id_2.to_lowercase().contains(adj)));
+    assert!(nouns.iter().any(|&noun| id_2.to_lowercase().contains(noun)));
 }
