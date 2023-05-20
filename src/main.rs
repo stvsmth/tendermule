2ea34f70d56dcfd293e3c50c5ec6b4189621d328
stvsmth <stvsmth@gmail.com>
2023-05-20 11:08:33 -0600

    Add parameter validation via clap_num.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use clap::Parser;
-use std::process::exit;
+use clap_num::number_range;
 use tendermule::generate_ids;
 mod words;
 use words::adjs;
 use words::nouns;
+
+fn valid_max_count(s: &str) -> Result<usize, String> {
+    number_range(s, 1, 1_000_000)
+}
+
+fn valid_id_len(s: &str) -> Result<u8, String> {
+    number_range(s, 8, 255)
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -17,21 +25,16 @@ struct Args {
     suffix: String,
 
     /// Number of unique identifiers to generate
-    #[arg(short, long, default_value_t = 1)]
+    #[arg(short, long, default_value_t = 1, value_parser=valid_max_count)]
     count: usize,
 
     /// Maximum length of the final identifier
-    #[arg(short, long, default_value_t = 16)]
+    #[arg(short, long, default_value_t = 16, value_parser=valid_id_len)]
     max_length: usize,
 }
 
 fn main() {
     let args = Args::parse();
-
-    if args.count == 0 {
-        eprintln!("Error: count must be greater than 0");
-        exit(128);
-    }
 
     let adjs = adjs::ADJS;
     let nouns = nouns::NOUNS;
