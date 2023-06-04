abbc0f5447f843bf6bff03d351962d7a4705e287
stvsmth <stvsmth@gmail.com>
2023-06-04 10:05:46 -0600

    Implement replay of fuzzed crash.

--- src/replay.rs
+++ src/replay.rs
@@ -1,8 +1,9 @@
-use std::io::Read;
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Read;
 use std::process::Command;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct FuzzData {
     prefix: String,
     suffix: String,
@@ -10,36 +11,58 @@ struct FuzzData {
     max_length: u8,
 }
 
+fn read_json_file(path: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn parse_json<T>(json_str: &str) -> Result<T, serde_json::Error>
+where
+    T: serde::de::DeserializeOwned,
+{
+    let value: T = serde_json::from_str(json_str)?;
+    Ok(value)
+}
+
 fn main() {
-    // Read the binary data from stdin
-    let mut data = Vec::new();
-    std::io::stdin().read_to_end(&mut data).unwrap();
+    let file_path = "test.json";
+    match read_json_file(file_path) {
+        Ok(contents) => match parse_json::<FuzzData>(&contents) {
+            Ok(fuzz_data) => {
+                println!("Parsed JSON: {:?}", fuzz_data);
+                let args = vec![
+                    "--prefix".to_string(),
+                    fuzz_data.prefix,
+                    "--suffix".to_string(),
+                    fuzz_data.suffix,
+                    "--count".to_string(),
+                    fuzz_data.count.to_string(),
+                    "--max-length".to_string(),
+                    fuzz_data.max_length.to_string(),
+                ];
 
-    // Deserialize the data into our FuzzData structure
-    let fuzz_data: Result<FuzzData, _> = bincode::deserialize(&data);
+                println!("Running command: {:?}", args);
+                let output = Command::new("target/debug/tendermule")
+                    .args(&args)
+                    .output()
+                    .expect("Failed to execute command");
 
-    if let Ok(fuzz_data) = fuzz_data {
-        // Prepare the arguments
-        let args = vec![
-            "--prefix".to_string(),
-            fuzz_data.prefix,
-            "--suffix".to_string(),
-            fuzz_data.suffix,
-            "--count".to_string(),
-            fuzz_data.count.to_string(),
-            "--max_length".to_string(),
-            fuzz_data.max_length.to_string(),
-        ];
-
-        // Run the target binary with the arguments
-        let output = Command::new("path/to/target/binary")
-            .args(&args)
-            .output()
-            .expect("Failed to execute command");
-
-        if !output.status.success() {
-            eprintln!("The program crashed!");
-            // Handle crash...
+                if output.status.success() {
+                    println!("The program ran successfully!");
+                    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+                } else {
+                    println!("The program failed!");
+                    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+                }
+            }
+            Err(err) => {
+                eprintln!("Failed to parse JSON: {}", err);
+            }
+        },
+        Err(err) => {
+            eprintln!("Failed to read file: {}", err);
         }
     }
 }
